8027d6241fd7349b45a0629d3c17b2c2a7c8a2df
eimantas.urbonas@foxconn4tech.com <eimantas.urbonas@foxconn4tech.com>
2017-12-07 09:04:37 +0100

    testt

--- App3/MainPage.xaml.cs
+++ App3/MainPage.xaml.cs
@@ -17,6 +17,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace App3
 {
+    //test3
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
